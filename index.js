482bbe5de7745b8528feeee5f6ea04b192d11b92
Zion <amazion24@outlook.com>
2025-08-22 10:55:28 +0300

    Added read

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 
+const tasks = [];
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname +'/index.html')
 })
+
+app.get('/t',(req,res)=>{
+    res.status(200).json(tasks)
+})
+
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
